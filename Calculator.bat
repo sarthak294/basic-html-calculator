344eb2bea2b89c5706e2585d009a2c8613c6cf75
sarthak294 <91525816+sarthak294@users.noreply.github.com>
2022-04-22 15:02:01 +0530

    Add files via upload

---
+++ Calculator.bat
@@ -0,0 +1,37 @@
+<html>
+    <head>
+        <title>Calculator</title>
+    </head>
+    <body>
+<div>
+    <input type="text" id="txt1" style="width: 100%; height: 40px;" />
+    <table>
+<tr>
+    <td><input type="button" value="7" /></td>
+    <td><input type="button" value="8" /></td>
+    <td><input type="button" value="9" /></td>
+    <td><input type="button" value="/" /></td>
+</tr>
+<tr>
+    <td><input type="button" value="4" /></td>
+    <td><input type="button" value="5" /></td>
+    <td><input type="button" value="6" /></td>
+    <td><input type="button" value="X" /></td>
+</tr>
+<tr>
+    <td><input type="button" value="1" /></td>
+    <td><input type="button" value="2" /></td>
+    <td><input type="button" value="3" /></td>
+    <td><input type="button" value="-" /></td>
+
+</tr>
+<tr>
+    <td><input type="button" value="0" /></td>
+    <td><input type="button" value="." /></td>
+    <td><input type="button" value="==" /></td>
+    <td><input type="button" value="+" /></td>
+</tr>
+   </table>
+</div>
+    </body>
+</html>
